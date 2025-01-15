9445aa0eed473ae39c786775884f579640fe9f73
TusharAbhinav <abhinav.tushar@iesoftek.com>
2025-01-15 16:47:50 +0530

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
 import QueryClientWrapper from "./queryClient";
+import { Analytics } from "@vercel/analytics/react"
 
 export const metadata: Metadata = {
   title: "GeekSpill",
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body>
         <QueryClientWrapper>{children}</QueryClientWrapper>
         <Toaster />
+        <Analytics/>
       </body>
     </html>
   );
